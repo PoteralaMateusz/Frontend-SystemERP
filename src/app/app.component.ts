a023bb477f7416b3f49205e17e77385bd5c934dc
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-30 13:05:09 +0200

    add dashboard menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { faSignOutAlt,faUserAlt,faBriefcase,faChartBar,faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend-SystemERP';
+  faSignOut = faSignOutAlt;
+  faUser = faUserAlt;
+  faBriefcase = faBriefcase;
+  faTachometer = faTachometerAlt;
+  protected readonly faChartBar = faChartBar;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {authInterceptorProviders} from "./_helpers/auth.interceptor";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -19,9 +20,11 @@ import {authInterceptorProviders} from "./_helpers/auth.interceptor";
     HttpClientModule,
     CommonModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
