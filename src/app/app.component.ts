e3604de59d4b317a65f150cecf2ef3ea8e2e8333
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-30 19:47:47 +0200

    change in menu and add main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { faSignOutAlt,faUserAlt,faBriefcase,faChartBar,faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from "./_services/token-storage.service";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Frontend-SystemERP';
-  faSignOut = faSignOutAlt;
-  faUser = faUserAlt;
-  faBriefcase = faBriefcase;
-  faTachometer = faTachometerAlt;
-  protected readonly faChartBar = faChartBar;
+
+  constructor(private tokenStorageService:TokenStorageService, private router:Router) {
+  }
+  ngOnInit(): void {
+    if (!this.tokenStorageService.isLoggedIn()){
+      this.router.navigate(["./login"])
+    }
+  }
 }
