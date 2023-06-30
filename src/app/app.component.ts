47a54d098105120d231d00afda9a644e2bdfd3f6
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-30 19:56:40 +0200

    move home page from index to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {TokenStorageService} from "./_services/token-storage.service";
-import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'Frontend-SystemERP';
 
-  constructor(private tokenStorageService:TokenStorageService, private router:Router) {
-  }
-  ngOnInit(): void {
-    if (!this.tokenStorageService.isLoggedIn()){
-      this.router.navigate(["./login"])
-    }
-  }
 }
