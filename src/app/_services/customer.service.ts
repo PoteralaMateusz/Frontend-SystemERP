dc8c4582cf320a09d82056fc2c3f3d0faa6e5e34
MateuszPoterala <poteralamateusz@gmail.com>
2023-07-01 18:11:54 +0200

    [add] add CRUD feature for Customer

--- src/app/_services/customer.service.ts
+++ src/app/_services/customer.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
 import {Customer} from "../model/customer";
 
 const CUSTOMER_API = "/url/customers";
@@ -8,14 +8,55 @@ const CUSTOMER_API = "/url/customers";
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
+
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  getAllCustomers(): Observable<Customer[]>{
-    return this.http.get<Customer[]>(`${CUSTOMER_API}`,httpOptions);
+  getAllCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${CUSTOMER_API}`, httpOptions);
+  }
+
+  addCustomer(name: string, street: string, houseNumber: number, city: string, zipcode: number, country: string): Observable<any> {
+    return this.http.post(`${CUSTOMER_API}`, {
+      name: name,
+      country: country,
+      city: city,
+      street: street,
+      houseNumber: houseNumber,
+      zipCode: zipcode
+    }, httpOptions).pipe(
+      tap(response => {
+        console.log(response);
+      })
+    );
+  }
+
+  updateCustomer(id: number, name: string, street: string, houseNumber: number, city: string, zipcode: number, country: string): Observable<any> {
+    return this.http.patch(`${CUSTOMER_API}/${id}`, {
+      name: name,
+      country: country,
+      city: city,
+      street: street,
+      houseNumber: houseNumber,
+      zipCode: zipcode
+    }, httpOptions).pipe(
+      tap(response => {
+        console.log(response);
+      })
+    );
+  }
+
+  deleteCustomer(id: number):Observable<any>{
+    return this.http.delete(`${CUSTOMER_API}/${id}`,httpOptions)
+      .pipe(
+      tap(response => {
+        console.log(response);
+      })
+      );
   }
 }
--- src/app/customer-info/customer-info.component.ts
+++ src/app/customer-info/customer-info.component.ts
@@ -2,23 +2,32 @@ import {Component, OnInit} from '@angular/core';
 import {Customer} from "../model/customer";
 import {CustomerService} from "../_services/customer.service";
 import {TokenStorageService} from "../_services/token-storage.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customer-info',
   templateUrl: './customer-info.component.html',
   styleUrls: ['./customer-info.component.css']
 })
-export class CustomerInfoComponent implements OnInit{
+export class CustomerInfoComponent implements OnInit {
   public customers = {} as Customer[];
   public isLogged = false;
+  public toUpdate = false;
+  form: any = {
+    id: null,
+    name: null,
+    street: null,
+    houseNumber: null,
+    city: null,
+    zipcode: null,
+    country: null
+  };
+  operation = 'Add';
 
-  constructor(private customerService :CustomerService, private tokenStorageService:TokenStorageService) {
+  constructor(private customerService: CustomerService, private tokenStorageService: TokenStorageService) {
   }
 
   ngOnInit(): void {
-
-    if (this.tokenStorageService.isLoggedIn()){
+    if (this.tokenStorageService.isLoggedIn()) {
       this.isLogged = true;
       this.customerService.getAllCustomers().subscribe({
         next: data => {
@@ -30,4 +39,47 @@ export class CustomerInfoComponent implements OnInit{
     console.log(this.isLogged);
   }
 
+  addCustomer() {
+    const {name, street, houseNumber, city, zipcode, country} = this.form;
+    if (this.toUpdate) {
+      this.customerService.updateCustomer(this.form.id, name, street, houseNumber, city, zipcode, country).subscribe()
+      this.toUpdate = false;
+      window.location.reload();
+    } else {
+      this.customerService.addCustomer(name, street, houseNumber, city, zipcode, country).subscribe();
+      window.location.reload();
+    }
+  }
+
+  updateCustomer(customer: Customer) {
+    this.form = {
+      id: customer.customerId,
+      name: customer.name,
+      street: customer.street,
+      houseNumber: customer.houseNumber,
+      city: customer.city,
+      zipcode: customer.zipCode,
+      country: customer.country
+    };
+    this.operation = 'Save';
+    this.toUpdate = true;
+  }
+
+  deleteCustomer(customer: Customer) {
+    this.customerService.deleteCustomer(customer.customerId).subscribe();
+    window.location.reload();
+  }
+
+  cancelUpdate() {
+    this.toUpdate = false;
+    this.form = {
+      id: null,
+      name: null,
+      street: null,
+      houseNumber: null,
+      city: null,
+      zipcode: null,
+      country: null
+    }
+  }
 }
