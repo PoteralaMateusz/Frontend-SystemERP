8ceaecb2bf5a3691344b491b33d7f319d9a46e29
MateuszPoterala <poteralamateusz@gmail.com>
2023-07-13 18:23:45 +0200

    [add] add sorting to Order page

--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -9,20 +9,52 @@ import {Router} from "@angular/router";
   styleUrls: ['./order-page.component.css']
 })
 export class OrderPageComponent implements OnInit{
+  allOrders:Order[] = [];
   orders:Order[] = [];
   form: any;
+  showFinished = false;
+  sortType: string = 'orderDate';
 
   constructor(private orderService:OrderService, private router:Router) {
   }
   ngOnInit(): void {
     this.orderService.getAllOrders().subscribe({
       next: data => {
-        this.orders = data;
+        this.allOrders = data;
+        this.orders = this.allOrders.filter(order => order.finishDate == null);
+        this.sortChange();
       }
     });
   }
 
   showOrderDetails(id:number) {
     this.router.navigate(['order-details', {id: id}]);
+  }
+
+  showFinishedOrders() {
+    this.showFinished = !this.showFinished;
+    this.setOrdersByFinished();
+  }
+  setOrdersByFinished(){
+    if (this.showFinished) {
+      this.orders = this.allOrders.filter(a => a.finishDate != null);
+    }else {
+      this.orders = this.allOrders.filter(a => a.finishDate == null);
+    }
+  }
+
+  sortChange() {
+    this.setOrdersByFinished();
+    if (this.sortType == 'orderDate'){
+      this.orders.sort((a, b) => a.orderDate >= b.orderDate ? -1 : -1);
+    }
+
+    if (this.sortType == 'deadline'){
+      this.orders.sort((a, b) => a.deadline >= b.deadline ? 1 : -1);
+    }
+
+    if (this.sortType == 'price'){
+      this.orders.sort((a, b) => a.price >= b.price ? 1 : -1);
+    }
   }
 }
