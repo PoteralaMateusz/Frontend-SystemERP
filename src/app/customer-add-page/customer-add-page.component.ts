40736630120c59afd554728e21a51974d69e206a
MateuszPoterala <poteralamateusz@gmail.com>
2023-08-03 19:19:22 +0200

    [add] add validation and customer add/update page

--- src/app/customer-add-page/customer-add-page.component.ts
+++ src/app/customer-add-page/customer-add-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CustomerService} from "../_services/customer.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-customer-add-page',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-add-page.component.css']
 })
 export class CustomerAddPageComponent {
+  customer: any = {
+    customerId: null,
+    name: null,
+    country: null,
+    city: null,
+    street: null,
+    houseNumber: null,
+    zipCode: null
+  };
+  params: any;
+  addOperation: boolean = true;
+
+  constructor(private customerService: CustomerService, private router: Router, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.params = params;
+      if (this.params.customerId != null) {
+        this.addOperation = false;
+        this.customerService.getCustomerById(this.params.customerId).subscribe({
+          next: data => {
+            this.customer = data;
+          }
+        });
+      } else {
+        this.addOperation = true;
+      }
+    });
+  }
+
+  save() {
+    const {name, street, houseNumber, city, zipCode, country} = this.customer;
+    this.customerService.addCustomer(name, street, houseNumber, city, zipCode, country).subscribe({
+      next: date => {
+        this.router.navigate(['customers']);
+      }
+    });
+  }
+  saveChanges() {
+    const {name, street, houseNumber, city, zipCode, country} = this.customer;
+    this.customerService.updateCustomer(this.params.customerId, name, street, houseNumber, city, zipCode, country).subscribe({
+      next: date => {
+        this.router.navigate(['customers']);
+      }
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['customers']);
+  }
+
 
 }
--- src/app/customer-page/customer-page.component.ts
+++ src/app/customer-page/customer-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Customer} from "../model/customer";
 import {CustomerService} from "../_services/customer.service";
 import {TokenStorageService} from "../_services/token-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customer-info',
@@ -11,19 +12,8 @@ import {TokenStorageService} from "../_services/token-storage.service";
 export class CustomerPageComponent implements OnInit {
   public customers: Customer[] = [];
   public isLogged = false;
-  public toUpdate = false;
-  form: any = {
-    id: null,
-    name: null,
-    street: null,
-    houseNumber: null,
-    city: null,
-    zipcode: null,
-    country: null
-  };
-  operation = 'Add';
 
-  constructor(private customerService: CustomerService, private tokenStorageService: TokenStorageService) {
+  constructor(private customerService: CustomerService, private tokenStorageService: TokenStorageService, private router:Router) {
   }
 
   ngOnInit(): void {
@@ -40,29 +30,11 @@ export class CustomerPageComponent implements OnInit {
   }
 
   addCustomer() {
-    const {name, street, houseNumber, city, zipcode, country} = this.form;
-    if (this.toUpdate) {
-      this.customerService.updateCustomer(this.form.id, name, street, houseNumber, city, zipcode, country).subscribe()
-      this.toUpdate = false;
-      window.location.reload();
-    } else {
-      this.customerService.addCustomer(name, street, houseNumber, city, zipcode, country).subscribe();
-      window.location.reload();
-    }
+    this.router.navigate(['add-customer']);
   }
 
   updateCustomer(customer: Customer) {
-    this.form = {
-      id: customer.customerId,
-      name: customer.name,
-      street: customer.street,
-      houseNumber: customer.houseNumber,
-      city: customer.city,
-      zipcode: customer.zipCode,
-      country: customer.country
-    };
-    this.operation = 'Save';
-    this.toUpdate = true;
+    this.router.navigate(['add-customer', {customerId: customer.customerId}]);
   }
 
   deleteCustomer(customer: Customer) {
@@ -70,17 +42,4 @@ export class CustomerPageComponent implements OnInit {
     window.location.reload();
   }
 
-  cancelUpdate() {
-    this.toUpdate = false;
-    this.operation = 'Add';
-    this.form = {
-      id: null,
-      name: null,
-      street: null,
-      houseNumber: null,
-      city: null,
-      zipcode: null,
-      country: null
-    }
-  }
 }
